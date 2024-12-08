8407005a7004742998a52871fe94582781602711
Wei Ning Liu <v-liuweining@microsoft.com>
2024-12-08 17:20:31 +0800

    update scripts

---
+++ others/train_scripts/val_bug_imgs_2.py
@@ -0,0 +1,132 @@
+import json
+from PIL import Image, ImageDraw, ImageFont
+
+from paddlex import create_pipeline
+import json
+import numpy as np
+import os
+import datetime
+
+class json_serialize(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+def loadFontMap():
+    with open('./CharFontMapping.json', 'r', encoding='utf8') as user_file:
+        file_contents = user_file.read()
+        parsed_json = json.loads(file_contents)
+        return parsed_json
+
+def add_white_area_to_image(image_path):
+    original_image = Image.open(image_path)
+    original_width, original_height = original_image.size
+    new_width = original_width * 2
+    new_height = original_height
+    new_image = Image.new("RGB", (new_width, new_height), (255, 255, 255))
+    new_image.paste(original_image, (0, 0))
+    return (new_image,original_width,original_height)
+
+def find_character_value(data, character):
+    return next((v for k, v in data.items() if character in k), character)
+
+def read_val_data():
+    with open(f"{root_folder}/train.txt","r",encoding="utf-8") as f:
+        content = f.read()
+        return content
+
+def get_result_stream(file_path):
+    return open(file_path, 'w', encoding='utf-8')
+
+def parse_ocr_result(ocrresult, source_image_path):
+    global root_folder
+    merged_data = [
+        {
+            "dt_poly": poly,
+            "dt_score": score,
+            "rec_text": text,
+            "rec_score": rec_score,
+        }
+        for poly, score, text, rec_score in zip(
+            ocrresult["dt_polys"], ocrresult["dt_scores"], ocrresult["rec_text"], ocrresult["rec_score"]
+        )
+    ]
+    newimg, original_width, original_height = add_white_area_to_image(source_image_path)
+    draw = ImageDraw.Draw(newimg)
+    FONT_MAPPING = loadFontMap()
+    for d in merged_data:
+        fill_color = (255, 0, 0) #red #(255, 182, 193)
+        if float(d['rec_score']) > 0.999:
+            fill_color = (0, 255, 0) #green
+        if float(d['rec_score']) > 0.9:
+            fill_color = (191, 255, 0) #light green
+        elif float(d['rec_score']) > 0.8:
+            fill_color = (255, 255, 0) #yellow
+        elif float(d['rec_score']) > 0.5:
+            fill_color = (255, 128, 0) #orange
+        # draw detected text range
+        points_tuple_d = [(x, y) for x, y in d["dt_poly"]]
+        points_tuple_d.append(points_tuple_d[0])
+        draw.line(points_tuple_d, fill=fill_color, width=2)
+
+        # draw text range on white area
+        points_tuple = [(x + original_width, y) for x, y in d["dt_poly"]]
+        points_tuple.append(points_tuple[0])
+        draw.line(points_tuple, fill=fill_color, width=2)
+
+        # draw text
+        font_size = max(points_tuple[2][1] - points_tuple[0][1] - 2, 1)
+        text_x = start_x = points_tuple[0][0]
+        text_y = start_y = points_tuple[0][1]
+        for index, c in enumerate(list(d["rec_text"])):
+            target_font = ImageFont.truetype(FONT_MAPPING.get(c, "arial.ttf"), font_size)
+            rollback_c = IMAGE_CHARS_MAPPING.get(c, {"word": c})["word"]
+            bbox = draw.textbbox((text_x, text_y), rollback_c, font=target_font)
+            draw.text((text_x, text_y), rollback_c, fill="black", font=target_font, spacing=0)
+            text_x = text_x + (bbox[2] - bbox[0])
+            text_y = start_y
+    from pathlib import Path
+    new_file_path = f"{root_folder}/compared/{os.path.basename(source_image_path)}.parsed{Path(source_image_path).suffix}"
+    newimg.save(new_file_path)
+
+def loadImageCharsMapping():
+    with open('./files/rollback_characters.json', "r", encoding="utf8") as rj:
+        rjson = json.loads(rj.read())
+        return rjson
+
+IMAGE_CHARS_MAPPING = loadImageCharsMapping()
+root_folder = "./bug_imgs_2"
+compare_result_file = None
+compare_result_file = get_result_stream(f"{root_folder}/result_paddleocr_{datetime.datetime.now().strftime('%Y%m%dT%H%M')}.csv")
+os.makedirs(f"{root_folder}/compared", exist_ok=True)
+
+pipeline = create_pipeline(pipeline="../configs/pipeline/OCR.yaml")
+for filepath, dirnames, filenames in os.walk(root_folder):
+    for filename in filenames:
+        f_name = f"{filepath}/{filename}"
+        if (f_name.endswith(".jpg") or f_name.endswith(".png")) and (not 'parsed' in f_name):
+            print(f_name)
+            output = pipeline.predict(f_name)
+            for res in output:
+                #res.save_to_img(f"{root_folder}/compared/")
+                predict_text = '<;>'.join(res["rec_text"])
+                # res.save_to_json()
+                with open(f"{root_folder}/compared/{filename}.json", "w", encoding="utf-8") as f:
+                    f.write(json.dumps(res, cls=json_serialize, ensure_ascii=False))
+
+                target_list = []
+                for c in list(predict_text):
+                    target_list.append(IMAGE_CHARS_MAPPING.get(c, {"word": c})["word"])
+
+                compare_result_file.write(f"{f_name},{predict_text},{''.join(target_list)}\n")
+                compare_result_file.flush()
+
+                parse_ocr_result(res, f_name)
+
+
+
---
+++ others/train_scripts/val_imgs_2.py
@@ -0,0 +1,98 @@
+from difflib import SequenceMatcher
+
+from paddlex import create_pipeline
+import json
+import numpy as np
+import os
+import datetime
+
+class json_serialize(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+root_folder = "./ocrval_2"
+compare_result_file = None
+def read_val_data():
+    with open(f"{root_folder}/train.txt","r",encoding="utf-8") as f:
+        content = f.read()
+        return content
+
+def get_result_stream(file_path):
+    # global REBUILDCSV
+    # if os.path.exists(file_path) and not REBUILDCSV:
+    #     return open(file_path, 'a', encoding='utf-8')
+    # else:
+    #     return open(file_path, 'w', encoding='utf-8')
+    return open(file_path, 'w', encoding='utf-8')
+
+TINDEX = datetime.datetime.now().strftime('%Y%m%dT%H%M')
+compare_result_file = get_result_stream(f"{root_folder}/result_paddleocr_{TINDEX}.csv")
+os.makedirs(f"{root_folder}/compared", exist_ok=True)
+
+pipeline = create_pipeline(pipeline="../configs/pipeline/OCR.yaml")
+content = read_val_data().splitlines()
+sorted_result = []
+
+for line in content:
+    file,val = line.split('\t')
+    print(file, val)
+    output = pipeline.predict(f"{root_folder}/{file}")
+    for res in output:
+        #res.save_to_img(f"{root_folder}/compared/")
+        predict_text = ''.join(res["rec_text"])
+        # res.save_to_json()
+        with open(f"{root_folder}/compared/{file.split('/')[1]}.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(res, cls=json_serialize, ensure_ascii=False))
+
+        ratio = SequenceMatcher(None, val, predict_text).ratio()
+        compare_result_file.write(f"{file},{ratio},'{val}','{predict_text}'\n")
+        sorted_result.append({
+            "file":file,
+            "ratio":ratio,
+            "val":val,
+            "predict_text":predict_text
+        })
+        compare_result_file.flush()
+
+def extract_time(json):
+    try:
+        # Also convert to int since update_time will be string.  When comparing
+        # strings, "10" is smaller than "2".
+        return float(json['ratio'])
+    except KeyError:
+        return 0
+
+def find_character_value(data, character):
+    return next((v for k, v in data.items() if character in k), character)
+
+with open(f"{root_folder}/result_paddleocr_{TINDEX}_sorted.csv", 'w', encoding='utf-8') as w, open('./files/rollback_characters.json', 'r', encoding='utf-8') as r:
+    EXCEPT_CHARS_RALLBACK = json.loads(r.read())
+    sorted_result.sort(key=extract_time, reverse=True)
+    for i in sorted_result:
+        w.write(f"{i['file']},{i['ratio']},'{i['val']}','{i['predict_text']}'\n")
+
+    ratio_100 = [ d for d in sorted_result if d['ratio'] == 1]
+    ratio_90 = [ d for d in sorted_result if d['ratio'] >= 0.9 and d['ratio'] < 1]
+    ratio_80 = [ d for d in sorted_result if d['ratio'] >= 0.8 and d['ratio'] < 0.9]
+    ratio_0 = [d for d in sorted_result if d['ratio'] ==0]
+
+    w.write(f"Total,{len(sorted_result)},len_100,{len(ratio_100)},len_90,'{len(ratio_90)}',len_80,'{len(ratio_80)}',len_0,{len(ratio_0)}\n")
+    char_list = []
+    for i in ratio_0:
+        char_list.extend(list(i['val']))
+        char_list.extend(list(i['predict_text']))
+
+    target_list = []
+    for c in list(set(char_list)):
+        real_char = EXCEPT_CHARS_RALLBACK.get(c, {"word": c})["word"]
+        target_list.append(real_char)
+
+    w.write("ratio_0_chars:\n")
+    w.write(f"{''.join(target_list)}\n")
+    w.write(f"{''.join(list(set(char_list)))}\n")
